10e75574501ae6bed2b1f5c711c2ffe9519cf53c
Hleb Rubanau <g.rubanau@gmail.com>
2019-11-05 12:41:53 +0300

    add setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='wsid-core',  
+     version='0.0.1',
+     scripts=[] ,
+     author="Hleb Rubanau",
+     author_email="contact@rubanau.com",
+     description="Implementation of WSID authentication mechanism",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/hleb-rubanau/wsid-core",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+)
+
